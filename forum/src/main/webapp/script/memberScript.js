a580d2409061ad6803bc2eac08234ff67712d7e2
Sscherzo <90139096+Sscherzo@users.noreply.github.com>
2022-11-05 22:28:26 +0900

    Update memberScript.js

--- forum/src/main/webapp/script/memberScript.js
+++ forum/src/main/webapp/script/memberScript.js
@@ -6,11 +6,11 @@ function checkLogin() {
 	if(!frm.id.value) {
 		alert("아이디를 입력하세요");
 		frm.id.focus();
-		return false;
+		
 	} else if(!frm.pw.value) {
 		alert("패스워드를 입력하세요");
 		frm.pw.focus();
-		return false;
+		
 	}else{
 		frm.submit();
 	}
